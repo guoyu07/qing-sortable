90c420ff331709e0604ed27fa61e088ddca431cd
frankfang <frankfang1990@gmail.com>
2016-10-19 09:53:19 +0800

    init

---
+++ dist/qing-sortable.js
@@ -0,0 +1,60 @@
+/**
+ * qing-sortable v0.0.1
+ * http://mycolorway.github.io/qing-sortable
+ *
+ * Copyright Mycolorway Design
+ * Released under the MIT license
+ * http://mycolorway.github.io/qing-sortable/license.html
+ *
+ * Date: 2016-10-18
+ */
+;(function(root, factory) {
+  if (typeof module === 'object' && module.exports) {
+    module.exports = factory(require('jquery'),require('qing-module'));
+  } else {
+    root.QingSortable = factory(root.jQuery,root.QingModule);
+  }
+}(this, function ($,QingModule) {
+var define, module, exports;
+var b = require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"qing-sortable":[function(require,module,exports){
+var QingSortable,
+  extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+  hasProp = {}.hasOwnProperty;
+
+QingSortable = (function(superClass) {
+  extend(QingSortable, superClass);
+
+  QingSortable.opts = {
+    el: null
+  };
+
+  function QingSortable(opts) {
+    QingSortable.__super__.constructor.apply(this, arguments);
+    this.el = $(this.opts.el);
+    if (!(this.el.length > 0)) {
+      throw new Error('QingSortable: option el is required');
+    }
+    this.opts = $.extend({}, QingSortable.opts, this.opts);
+    this._render();
+    this.trigger('ready');
+  }
+
+  QingSortable.prototype._render = function() {
+    this.el.append("<p>This is a sample component.</p>");
+    return this.el.addClass(' qing-sortable').data('qingSortable', this);
+  };
+
+  QingSortable.prototype.destroy = function() {
+    return this.el.empty().removeData('qingSortable');
+  };
+
+  return QingSortable;
+
+})(QingModule);
+
+module.exports = QingSortable;
+
+},{}]},{},[]);
+
+return b('qing-sortable');
+}));
