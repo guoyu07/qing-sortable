6c701471f9bd3e10717745d5640ea61ce836d44a
frankfang <frankfang1990@gmail.com>
2016-10-20 15:23:37 +0800

    out of container

--- dist/qing-sortable.js
+++ dist/qing-sortable.js
@@ -6,7 +6,7 @@
  * Released under the MIT license
  * http://mycolorway.github.io/qing-sortable/license.html
  *
- * Date: 2016-10-19
+ * Date: 2016-10-20
  */
 ;(function(root, factory) {
   if (typeof module === 'object' && module.exports) {
@@ -17,7 +17,7 @@
 }(this, function ($,QingModule) {
 var define, module, exports;
 var b = require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"qing-sortable":[function(require,module,exports){
-var QingSortable, active, placeholder,
+var QingSortable, active, enter, placeholder, throttle,
   extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
   hasProp = {}.hasOwnProperty;
 
@@ -25,10 +25,31 @@ active = null;
 
 placeholder = null;
 
+enter = false;
+
+throttle = function(fn, duration) {
+  var timer;
+  if (duration == null) {
+    duration = 100;
+  }
+  timer = null;
+  return function() {
+    var args, context;
+    context = this;
+    args = arguments;
+    window.clearTimeout(timer);
+    return timer = window.setTimeout(function() {
+      return fn.apply(context, args);
+    }, duration);
+  };
+};
+
 QingSortable = (function(superClass) {
   extend(QingSortable, superClass);
 
   QingSortable.opts = {
+    container: null,
+    connect: null,
     sortable: null,
     customDragImage: false
   };
@@ -54,13 +75,19 @@ QingSortable = (function(superClass) {
     this.sortable.on('dragstart.qingSortable', (function(_this) {
       return function(e) {
         var $item, dragImage;
-        $item = $(e.currentTarget).addClass('qing-sortable-active');
+        _this.dragging = true;
+        $item = $(e.currentTarget);
         if (_this.opts.customDragImage) {
           dragImage = $item.clone().addClass('qing-sortable-dragimage');
-          dragImage.appendTo('body');
-          e.originalEvent.dataTransfer.setDragImage(dragImage[0], dragImage.width() / 2, dragImage.height() / 2);
+          dragImage.prependTo($item.parent());
+          e.originalEvent.dataTransfer.setDragImage(dragImage[0], e.pageX - $item.offset().left, e.pageY - $item.offset().top);
         }
+        _this.relative = {
+          x: e.pageX - $item.offset().left,
+          y: e.pageY - $item.offset().top
+        };
         active = $item;
+        $item.addClass('qing-sortable-placeholder');
         return placeholder = $item.clone().attr('data-sort-placeholder', true);
       };
     })(this));
@@ -68,24 +95,95 @@ QingSortable = (function(superClass) {
       return function(e) {
         var $item;
         $item = $(e.currentTarget);
-        $item.removeClass('qing-sortable-active');
+        $item.removeClass('qing-sortable-placeholder');
+        $item.removeClass('xxx');
         if ($.contains(document, placeholder[0])) {
-          $(e.currentTarget).show().insertAfter(placeholder);
+          $item.insertAfter(placeholder);
           placeholder.detach();
         }
-        return active = null;
+        active = null;
+        return _this.dragging = false;
+      };
+    })(this));
+    $(this.opts.container).on('dragenter.qingSortable', function() {
+      return enter = true;
+    });
+    $(this.opts.container).on('dragleave.qingSortable', function() {
+      return enter = false;
+    });
+    $(document).on('dragover.qingSortable', (function(_this) {
+      return function(e) {
+        var all, center, method, nearest, sorted;
+        if (_this.sortable.filter(e.target).length !== 0) {
+          return;
+        }
+        if ($(_this.opts.container).filter(e.target).length > 0) {
+          return;
+        }
+        if ($.contains($(_this.opts.container)[0], e.target)) {
+          return;
+        }
+        if (_this.opts.connect) {
+          if ($(_this.opts.connect).filter(e.target).length > 0) {
+            return;
+          }
+          if ($.contains($(_this.opts.connect)[0], e.target)) {
+            return;
+          }
+        }
+        if (enter) {
+          return;
+        }
+        if (!_this.dragging) {
+          return;
+        }
+        placeholder.detach();
+        center = {
+          x: e.pageX - _this.relative.x + active.outerWidth() / 2,
+          y: e.pageY - _this.relative.y + active.outerHeight() / 2
+        };
+        all = _this.sortable.map(function(index, el) {
+          var $el, c, delta, offset;
+          $el = $(el);
+          offset = $el.offset();
+          c = {
+            x: offset.left + $el.outerWidth() / 2,
+            y: offset.top + $el.outerHeight() / 2
+          };
+          delta = Math.sqrt(Math.pow(center.x - c.x, 2) + Math.pow(center.y - c.y, 2));
+          return {
+            delta: delta,
+            element: el,
+            center: c
+          };
+        });
+        sorted = all.sort(function(a, b) {
+          return a.delta - b.delta;
+        });
+        nearest = sorted[0].element;
+        method;
+        if (_this.opts.type === 'inline') {
+          method = center.x < sorted[0].center.x ? 'before' : 'after';
+        } else {
+          method = center.y < sorted[0].center.y ? 'before' : 'after';
+        }
+        return $(nearest)[method](placeholder).addClass('yyy').siblings().removeClass('yyy').end();
       };
     })(this));
     return this.sortable.on('dragenter.qingSortable', (function(_this) {
       return function(e) {
-        var $item, index, method;
+        var $item, index, inn, method;
         $item = $(e.currentTarget);
-        if ($item[0] !== active[0]) {
-          index = $.contains(document, placeholder[0]) ? placeholder.index() : active.index();
-          method = index < $item.index() ? 'after' : 'before';
-          $item[method](placeholder);
-          return active.hide();
+        inn = $(_this.opts.container).add(_this.opts.connect).filter(function(index, el) {
+          return $.contains(el, e.currentTarget);
+        });
+        if (!(inn.length > 0)) {
+          return;
         }
+        index = $.contains(document, placeholder[0]) ? placeholder.index() : active.index();
+        method = index < $item.index() ? 'after' : 'before';
+        $item[method](placeholder);
+        return active.addClass('xxx');
       };
     })(this));
   };
--- src/qing-sortable.coffee
+++ src/qing-sortable.coffee
@@ -1,9 +1,21 @@
 active = null
 placeholder = null
+enter = false
+throttle = (fn, duration=100) ->
+  timer = null
+  return ()->
+    context = @
+    args = arguments
+    window.clearTimeout timer
+    timer = window.setTimeout ()->
+      fn.apply context, args
+    , duration
 
 class QingSortable extends QingModule
 
   @opts:
+    container: null
+    connect: null
     sortable: null
     customDragImage: false
 
@@ -24,34 +36,97 @@ class QingSortable extends QingModule
 
   _bind: ->
     @sortable.on 'dragstart.qingSortable', (e) =>
-      $item = $(e.currentTarget).addClass('qing-sortable-active')
+      @dragging = true
+      $item = $(e.currentTarget)
       if @opts.customDragImage
         dragImage = $item.clone().addClass('qing-sortable-dragimage')
-        dragImage.appendTo('body')
+        dragImage.prependTo $item.parent()
         e.originalEvent.dataTransfer.setDragImage(
-          dragImage[0],dragImage.width()/2,dragImage.height()/2
+          dragImage[0],
+          e.pageX-$item.offset().left,
+          e.pageY-$item.offset().top
         )
+
+      @relative =
+        x: e.pageX-$item.offset().left
+        y: e.pageY-$item.offset().top
       active = $item
+      $item.addClass('qing-sortable-placeholder')
       placeholder = $item.clone().attr('data-sort-placeholder', true)
 
     @sortable.on 'dragend.qingSortable', (e) =>
       $item = $(e.currentTarget)
-      $item.removeClass 'qing-sortable-active'
+      $item.removeClass('qing-sortable-placeholder')
+      $item.removeClass 'xxx'
       if $.contains document, placeholder[0]
-        $(e.currentTarget).show().insertAfter(placeholder)
+        $item.insertAfter(placeholder)
         placeholder.detach()
       active = null
+      @dragging = false
+
+
+    $(@opts.container).on 'dragenter.qingSortable', ()->
+      enter = true
+    $(@opts.container).on 'dragleave.qingSortable', ()->
+      enter = false
+
+    $(document).on 'dragover.qingSortable', (e)=>
+      return if @sortable.filter(e.target).length isnt 0
+      return if $(@opts.container).filter(e.target).length > 0
+      return if $.contains($(@opts.container)[0], e.target)
+      if @opts.connect
+        return if $(@opts.connect).filter(e.target).length > 0
+        return if $.contains($(@opts.connect)[0], e.target)
+      return if enter
+      return unless @dragging
+
+      placeholder.detach()
+      center =
+        x: e.pageX - @relative.x + active.outerWidth() / 2
+        y: e.pageY - @relative.y + active.outerHeight() / 2
+
+
+      all =  @sortable.map (index, el)->
+        $el = $(el)
+        offset = $el.offset()
+        c =
+          x: offset.left + $el.outerWidth()/2
+          y: offset.top + $el.outerHeight()/2
+        delta = Math.sqrt(Math.pow((center.x - c.x),2) +
+          Math.pow((center.y - c.y),2))
+        delta: delta
+        element: el
+        center: c
+      sorted = all.sort (a,b)->
+        a.delta - b.delta
+      nearest = sorted[0].element
+
+      method
+      if @opts.type is 'inline'
+        method = if center.x < sorted[0].center.x then 'before' else 'after'
+      else
+        method = if center.y < sorted[0].center.y then 'before' else 'after'
+
+      $(nearest)[method](placeholder).addClass('yyy')
+      .siblings().removeClass('yyy').end()
 
     @sortable.on 'dragenter.qingSortable', (e)=>
       $item = $ e.currentTarget
-      unless $item[0] is active[0]
-        index = if $.contains document, placeholder[0]
-        then placeholder.index()
-        else active.index()
-        method = if index < $item.index()
-        then 'after' else 'before'
-        $item[method] placeholder
-        active.hide()
+
+      inn = $(@opts.container).add(@opts.connect).filter (index, el)->
+        $.contains(el, e.currentTarget)
+
+      return unless inn.length > 0
+
+      index = if $.contains document, placeholder[0]
+      then placeholder.index()
+      else active.index()
+
+      method = if index < $item.index()
+      then 'after' else 'before'
+      $item[method] placeholder
+
+      active.addClass('xxx')
 
   destroy: ->
     @sortable.off('.qingSortable')
